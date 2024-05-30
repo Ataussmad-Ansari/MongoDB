121e9b0cadab3848e41b06c103003c0dd8bc8d2f
Samad <sam.an.vns6@gmail.com>
2024-05-30 15:27:42 +0530

    update code

--- app/src/main/java/com/example/usemongodb/Category_Model.java
+++ app/src/main/java/com/example/usemongodb/Category_Model.java
@@ -1,2 +1,50 @@
-package com.example.usemongodb;public class Catagory_Model {
+package com.example.usemongodb;
+
+public class Category_Model {
+    String category_name;
+
+    int category_image;
+
+    public Category_Model(String category_name, int category_image) {
+        this.category_name = category_name;
+        this.category_image = category_image;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    public int getCategory_image() {
+        return category_image;
+    }
+
+    public void setCategory_image(int category_image) {
+        this.category_image = category_image;
+    }
+
+    /*String category_image;
+    public Category_Model(String category_name, String category_image) {
+        this.category_name = category_name;
+        this.category_image = category_image;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    public String getCategory_image() {
+        return category_image;
+    }
+
+    public void setCategory_image(String category_image) {
+        this.category_image = category_image;
+    }*/
 }
